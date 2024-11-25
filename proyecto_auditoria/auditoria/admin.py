a37b1a9b560ae9640a4fe5b9bc0a788326b18d12
KrlosPK <centille095@gmail.com>
2024-11-25 17:13:13 -0500

    feat: register new table

--- proyecto_auditoria/auditoria/admin.py
+++ proyecto_auditoria/auditoria/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Controles, Diseño, Validaciones_Diseño, Validaciones_Diseño_Diseño
+from .models import (
+    Controles,
+    Diseño,
+    Validaciones_Diseño,
+    Validaciones_Diseño_Diseño,
+    Auditores,
+)
 
 admin.site.register(Controles)
 admin.site.register(Diseño)
 admin.site.register(Validaciones_Diseño)
 admin.site.register(Validaciones_Diseño_Diseño)
+admin.site.register(Auditores)
