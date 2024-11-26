7fa3eb0f52b64e9587266c67a2880ee0ae9798f6
KrlosPK <centille095@gmail.com>
2024-11-25 20:04:04 -0500

    feat: update virtual environment

--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Temp Tech\Documents\Teker - Estudio\prueba-talento-b\virt
+set VIRTUAL_ENV=C:\Users\centi\Documents\Krlos\Proyectos\proyecto-auditoria\virt
 
 if not defined PROMPT set PROMPT=$P$G
 
