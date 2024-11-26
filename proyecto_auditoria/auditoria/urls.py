5f85cd3b1ecf56aff17a3e56736f7295d155f623
KrlosPK <centille095@gmail.com>
2024-11-25 21:59:29 -0500

    feat(encabezado): add basic template to update and save an encabezado

--- proyecto_auditoria/auditoria/urls.py
+++ proyecto_auditoria/auditoria/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.all_controles, name="home"),
     path("diseño/<str:codigo_control>", views.diseño, name="diseño"),
+    path("encabezado/<str:codigo_control>", views.encabezado, name="encabezado"),
 ]
--- proyecto_auditoria/auditoria/views.py
+++ proyecto_auditoria/auditoria/views.py
@@ -1,4 +1,4 @@
-from .models import Controles, Diseño
+from .models import Controles, Diseño, Encabezado
 from datetime import datetime
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -65,7 +65,9 @@ def diseño(request, codigo_control):
                 )
 
                 messages.success(request, "Diseño creado exitosamente.")
+
             diseño.save()
+
             return redirect("diseño", codigo_control=codigo_control)
         else:
             errores_formateados = "\n".join(errores.values())
@@ -78,4 +80,79 @@ def diseño(request, codigo_control):
         request,
         "diseño.html",
         {"control": control, "diseño": diseño, "errores": errores},
+    )
+
+
+@login_required(login_url="auth/login_user/")
+def encabezado(request, codigo_control):
+    control = get_object_or_404(Controles, codigo_control=codigo_control)
+
+    encabezado = Encabezado.objects.filter(control_id=control).first()
+
+    errores = {}
+
+    if request.method == "POST":
+        fecha_elaboracion = request.POST.get("production_date", "")
+        estado = request.POST.get("state", "")
+        total_horas_invertidas = request.POST.get("total_hours_invested", "")
+        recursos_consultados = request.POST.get("resources_consulted", "")
+
+        if not fecha_elaboracion:
+            errores["production_date"] = "El campo Fecha Elaboración es obligatorio."
+        if not estado:
+            errores["state"] = "El campo Estado es obligatorio."
+        if not total_horas_invertidas:
+            errores["total_hours_invested"] = (
+                "El total de horas invertidas es obligatorio."
+            )
+        if total_horas_invertidas and total_horas_invertidas < "0":
+            errores["total_hours_invested"] = (
+                "El total de horas invertidas debe ser mayor o igual a 0."
+            )
+        if not recursos_consultados:
+            errores["resources_consulted"] = (
+                "Los recursos consultados son obligatorios."
+            )
+        else:
+            try:
+                fecha_elaboracion = datetime.strptime(
+                    fecha_elaboracion, "%m/%d/%Y"
+                ).strftime("%Y-%m-%d")
+            except ValueError:
+                errores["production_date"] = "El formato de la fecha es inválido."
+
+        if not errores:
+            if encabezado:
+                encabezado.fecha_elaboracion = fecha_elaboracion
+                encabezado.estado = estado
+                encabezado.total_horas_invertidas = total_horas_invertidas
+                encabezado.recursos_consultados = recursos_consultados
+
+                messages.success(request, "Encabezado actualizado exitosamente.")
+
+            else:
+                encabezado = Encabezado(
+                    control_id=control.id,
+                    fecha_elaboracion=fecha_elaboracion,
+                    estado=estado,
+                    total_horas_invertidas=total_horas_invertidas,
+                    recursos_consultados=recursos_consultados,
+                )
+
+                messages.success(request, "Encabezado creado exitosamente.")
+
+            encabezado.save()
+
+            return redirect("encabezado", codigo_control=codigo_control)
+        else:
+            errores_formateados = "\n".join(errores.values())
+
+            messages.error(request, errores_formateados)
+
+            return redirect("encabezado", codigo_control=codigo_control)
+
+    return render(
+        request,
+        "encabezado.html",
+        {"control": control, "encabezado": encabezado, "errores": errores},
     )
